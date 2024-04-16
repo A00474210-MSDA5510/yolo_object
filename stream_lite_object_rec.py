85cca74c8a6bda0ef24196792847177a478de121
A00474210_MSDA5510 <zunyang.wang@smu.ca>
2024-04-16 15:22:11 -0300

    renamed file avoid confustion

--- stream_lite_object_rec.py
+++ stream_lite_object_rec.py
@@ -4,25 +4,30 @@ from ultralytics import YOLO
 import av
 import time
 import threading
+from queue import Queue
 
 st.title("OpenCV Filters on Video Stream")
 model = YOLO('yolov8n.pt')
 item_to_track = [45, 39, 41]
 
-if "frames" not in st.session_state:
-    st.session_state.frames = []
+
 
 lock = threading.Lock()
+img_container = {"img": None}
+if "frames" not in st.session_state:
+    st.session_state.frames = Queue()
 
 def transform(frame):
     img = frame.to_ndarray(format="bgr24")
     results = model.track(img, persist=True)
+    with lock:
+        img_container["img"] = results[0]
     img = results[0].plot()
     return av.VideoFrame.from_ndarray(img, format="bgr24")
 
 webrtc_ctx = webrtc_streamer(
     key="object-detection",
-    video_processor_factory=transform,
+    video_frame_callback=transform,
     sendback_audio=False,
     rtc_configuration={  # Add this line
         "iceServers": [{"urls": ["stun:stun2.l.google.com:19302"]}]},
@@ -35,19 +40,26 @@ if "frames" not in st.session_state:
 if "start_time" not in st.session_state:
     st.session_state['start_time'] = None
 
+if "queue_size" not in st.session_state:
+    st.session_state.queue_size = 0
+
 
 
 def time_calc(webrtc_ctx):
     if webrtc_ctx.state.playing:
         st.session_state.start_time = time.time()
-        st.write("IM PLAYING DEEZNUTS")
+        with lock:
+            img = img_container["img"]
+            st.session_state.frames.put(img)
+            st.session_state.queue_size += 1
+        if img is None:
+            pass
     elif not webrtc_ctx.state.playing and st.session_state.start_time != None:
         elapsed_time = time.time() - st.session_state.start_time
         st.session_state.start_time = None
         st.write(elapsed_time)
-        result = len(webrtc_ctx.video_processor.result_queue)
+        result = st.session_state.queue_size
         st.write(result)
-        st.write("HOHOHOHOH")
 
 time_calc(webrtc_ctx)
 st.write(webrtc_ctx.state.playing)
